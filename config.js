e6016a7a6c659c3d6fd0a6f3c53414165256b572
virgutiss-oss <virgutiss@icloud.com>
2026-01-24 21:51:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,2 @@
-// ⚠️ API-Football FREE plan konfiguracija
-// ❗ GitHub Pages tai MATYS VISI – naudok tik FREE key
-
 const API_KEY = "PASTE_YOUR_API_KEY_HERE";
 const API_HOST = "v3.football.api-sports.io";
