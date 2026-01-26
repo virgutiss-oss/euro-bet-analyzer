144b7573276c63d7cffc68b76b370d3fa3eedf7d
virgutiss-oss <virgutiss@icloud.com>
2026-01-26 19:05:05 +0200

    Update odds.js

--- api/odds.js
+++ api/odds.js
@@ -1,9 +1,6 @@
 export default async function handler(req, res) {
   try {
     const API_KEY = process.env.ODDS_API_KEY;
-    if (!API_KEY) {
-      return res.status(500).json({ error: "Missing API key" });
-    }
 
     const fetchSport = async (sport) => {
       const url = `https://api.the-odds-api.com/v4/sports/${sport}/odds?regions=eu&markets=h2h,totals&oddsFormat=decimal&apiKey=${API_KEY}`;
@@ -11,27 +8,13 @@ export default async function handler(req, res) {
       return await r.json();
     };
 
-    const data = {
-      football: [
-        ...(await fetchSport("soccer_epl")),
-        ...(await fetchSport("soccer_spain_la_liga")),
-        ...(await fetchSport("soccer_germany_bundesliga")),
-        ...(await fetchSport("soccer_italy_serie_a")),
-        ...(await fetchSport("soccer_france_ligue_one")),
-        ...(await fetchSport("soccer_uefa_champs_league"))
-      ],
-      basketball: [
-        ...(await fetchSport("basketball_nba")),
-        ...(await fetchSport("basketball_euroleague"))
-      ],
-      hockey: await fetchSport("icehockey_nhl"),
-      tennis: [
-        ...(await fetchSport("tennis_atp")),
-        ...(await fetchSport("tennis_wta"))
-      ]
-    };
+    const basketball = await fetchSport("basketball_nba");
+    const football = await fetchSport("soccer_epl");
 
-    res.status(200).json(data);
+    res.status(200).json({
+      basketball,
+      football
+    });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
