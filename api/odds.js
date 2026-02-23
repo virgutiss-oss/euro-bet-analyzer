6b7cb66fde57a615fd666d7bdeec778148aed0d5
virgutiss-oss <virgutiss@icloud.com>
2026-02-23 17:11:36 +0200

    Refactor error response structure in odds.js

--- api/odds.js
+++ api/odds.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     if (!response.ok) {
-      return res.status(500).json({ error: data });
+      return res.status(500).json(data);
     }
 
     res.status(200).json(data);
